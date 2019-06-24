a5ca50fba309655d113b5f47b11f6a55dee7ea90
Clemyxy <clemylepopor@gmail.com>
2019-06-25 00:24:46 +0200

    Video 28

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -2,7 +2,10 @@
 
 
 #include "Grabber.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
 
+#define OUT
 // Sets default values for this component's properties
 UGrabber::UGrabber()
 {
@@ -29,6 +32,11 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
+
+	UE_LOG(LogTemp, Warning, TEXT("The Player is at %s , looking at %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
 }
 
